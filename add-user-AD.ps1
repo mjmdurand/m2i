8ae8e756c3c635406795b8659480b0219db905a6
Marc Durand <mjm.durand@hotmail.fr>
2022-04-29 11:45:46 +0200

    adding more verbose

--- add-user-AD.ps1
+++ add-user-AD.ps1
@@ -11,35 +11,43 @@ if (Get-ADOrganizationalUnit -Filter 'distinguishedName -eq $rootOUpath') {
     Write-Host "$rootOUpath already exists." -ForegroundColor Red
   } 
   else {
+    Write-Host "$rootOUpath don't exists." -ForegroundColor green
+    Write-Host "Creating $rootOUpath OU" -ForegroundColor yellow
     #New-ADOrganizationalUnit -Name $rootOU -Path "DC=$DCdomain,DC=$DCtld"
   }
 
 #check if location OU is already created, creating it if not
 foreach($user in $data){
-    $locationOU = $data.lieu
+    $locationOU = $user.lieu
     $locationOUpath = "OU=$locationOU,OU=$rootOU,DC=$DCdomain,DC=$DCtld"
     if (Get-ADOrganizationalUnit -Filter 'distinguishedName -eq $locationOUpath') {
         Write-Host "$locationOU already exists." -ForegroundColor red
     } 
     else {
+        Write-Host "$locationOU don't exists." -ForegroundColor green
+        Write-Host "Creating $locationOU OU" -ForegroundColor yellow
         #New-ADOrganizationalUnit -Name $locationOU -Path $rootOUpath
     }
 
     #Checking if Users OU is already created, creating it if not
-    $locationOUpath = "OU=Users,OU=$locationOU,OU=$rootOU,DC=$DCdomain,DC=$DCtld"
+    $UserslocationOUpath = "OU=Users,OU=$locationOU,OU=$rootOU,DC=$DCdomain,DC=$DCtld"
     if (Get-ADOrganizationalUnit -Filter 'distinguishedName -eq $UserslocationOUpath') {
         Write-Host "Users OU already exists." -ForegroundColor red
     } 
     else {
+        Write-Host "Users don't exists." -ForegroundColor green
+        Write-Host "Creating Users OU" -ForegroundColor yellow
         #New-ADOrganizationalUnit -Name "Users" -Path $locationOUpath
     }
 
     #Checking if Computers OU is already created, creating it if not
-    $locationOUpath = "Ou=Computers,OU=$locationOU,OU=$rootOU,DC=$DCdomain,DC=$DCtld"
-    if (Get-ADOrganizationalUnit -Filter 'distinguishedName -eq $ComputesrlocationOUpath') {
+    $ComputerslocationOUpath = "Ou=Computers,OU=$locationOU,OU=$rootOU,DC=$DCdomain,DC=$DCtld"
+    if (Get-ADOrganizationalUnit -Filter 'distinguishedName -eq $ComputerslocationOUpath') {
         Write-Host "Computer OU already exists." -ForegroundColor red
     } 
     else {
+        Write-Host "Computers don't exists." -ForegroundColor green
+        Write-Host "Creating Computers OU" -ForegroundColor yellow
         #New-ADOrganizationalUnit -Name "Computers" -Path $locationOUpath
     }
 }

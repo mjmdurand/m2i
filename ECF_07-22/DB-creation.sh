aea7ae1258d94a00021a2f4920389d66bc5cb62a
marc durand <mjm.durand@hotmail.fr>
2022-07-27 19:55:17 +0200

    "Nextcloud" typo

--- ECF_07-22/DB-creation.sh
+++ ECF_07-22/DB-creation.sh
@@ -5,7 +5,7 @@ echo -e "\e[1;34m Create database for $NomSite.\e[0m"
 read -p "Database name : " dbname
 read -p "Database user : " user
 echo -e "\e[1;41mPlease do not use the same password for UNIX user and SQL user\e[0m"
-read -s -p "Nextcloud database user password : " dbpasswd
+read -s -p "Database user password : " dbpasswd
 
 sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${dbname} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 echo -e "\e[1;32m${dbname} database created.\e[0m"

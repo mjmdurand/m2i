2c4280d9fdd124c4e07d20048f4b23da1ffedcfc
mjmdurand <mjm.durand@hotmail.fr>
2022-07-28 09:36:42 +0200

    fix bad var name

--- ECF_07-22/DB-creation.sh
+++ ECF_07-22/DB-creation.sh
@@ -3,7 +3,7 @@
 #Creating new database
 echo -e "\e[1;34m Create database for $NomSite.\e[0m"
 read -p "Database name : " dbname
-read -p "Database user : " user
+read -p "Database user : " dbuser
 echo -e "\e[1;41mPlease do not use the same password for UNIX user and SQL user\e[0m"
 read -s -p "Database user password : " dbpasswd
 
